8536606b13f542fd9314bb7a0bfda49d26506827
oscelest <oscelest@gmail.com>
2019-04-01 18:59:08 +0200

    Added rarity condition.

--- Enums/ConditionOperator.cs
+++ Enums/ConditionOperator.cs
@@ -3,10 +3,10 @@ namespace FilterBuilder.Enums {
         private readonly string _name;
 
         public static readonly ConditionOperator EQUAL = new ConditionOperator("=");
-        public static readonly ConditionOperator LESS = new ConditionOperator("<=");
-        public static readonly ConditionOperator LESSOREQUAL = new ConditionOperator("<");
-        public static readonly ConditionOperator GREATER = new ConditionOperator(">");
-        public static readonly ConditionOperator GREATEROREQUAL = new ConditionOperator(">=");
+        public static readonly ConditionOperator LESS_THAN = new ConditionOperator("<=");
+        public static readonly ConditionOperator LESS_THAN_OR_EQUAL = new ConditionOperator("<");
+        public static readonly ConditionOperator GREATER_THAN = new ConditionOperator(">");
+        public static readonly ConditionOperator GREATER_THAN_OR_EQUAL = new ConditionOperator(">=");
 
         private ConditionOperator(string name) {
             this._name = name;
--- Filter/FilterCondition.cs
+++ Filter/FilterCondition.cs
@@ -5,26 +5,45 @@ using System.Windows.Media;
 using FilterBuilder.Enums;
 
 namespace FilterBuilder.Filter {
-    
     public class FilterCondition {
-
-        public Tuple<ConditionOperator, string> LinkedSockets;
-        public FilterCondition() {
-            var test = ConditionOperator.GREATER; // "<"
-        }
+        public Tuple<ConditionOperator, RarityCondition> LinkedSockets;
+        public FilterCondition() { }
 
         public void SetLinkedSockets(List<string> values) {
-            LinkedSockets = new Tuple<ConditionOperator, string>(ConvertOperator(values[0]), values[1]); 
+            LinkedSockets =
+                new Tuple<ConditionOperator, RarityCondition>(ConvertOperator(values[0]), ConvertRarity(values[1]));
         }
 
-        private ConditionOperator ConvertOperator(string op) {
-            switch (op) {
-                case "=":
-                    return ConditionOperator.EQUAL;
-                
-                default: throw new NotImplementedException();
+        private static RarityCondition ConvertRarity(string rarity) {
+            switch (rarity) {
+                case "Normal":
+                    return RarityCondition.NORMAL;
+                case "Magic":
+                    return RarityCondition.MAGIC;
+                case "Rare":
+                    return RarityCondition.RARE;
+                case "Unique":
+                    return RarityCondition.UNIQUE;
+                default:
+                    throw new NotImplementedException();
             }
         }
 
+        private static ConditionOperator ConvertOperator(string op) {
+            switch (op) {
+                case "=":
+                    return ConditionOperator.EQUAL;
+                case "<":
+                    return ConditionOperator.LESS_THAN;
+                case "<=":
+                    return ConditionOperator.LESS_THAN_OR_EQUAL;
+                case ">":
+                    return ConditionOperator.GREATER_THAN;
+                case ">=":
+                    return ConditionOperator.GREATER_THAN_OR_EQUAL;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
     }
 }
