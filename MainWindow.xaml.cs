f31a996cc1227defd671fa143acfd0bba2ee7fca
oscelest <oscelest@gmail.com>
2019-04-03 23:45:55 +0200

    Parser working copy.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,11 +7,6 @@ namespace FilterBuilder {
     public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
-
-            //Dummy Data
-            FilterList.Items.Add(new FilterFile(){Name = "NeverSink - 0 SOFT", Path = "C:\\Documents\\My Games\\Path of Exile"});
-            FilterList.Items.Add(new FilterFile(){Name = "NeverSink - 1 REGULAR", Path = "C:\\Documents\\My Games\\Path of Exile"});
-            FilterList.Items.Add(new FilterFile(){Name = "NeverSink - 2 SEMI-SCRICT", Path = "C:\\Documents\\My Games\\Path of Exile"});
         }
 
         public void Click_LoadFile(object sender, RoutedEventArgs e) {
@@ -19,21 +14,15 @@ namespace FilterBuilder {
 
             fileDialog.Filter = "filter files (*.filter)|*.filter";
             fileDialog.InitialDirectory = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents", "My Games", "Path of Exile");
-            
+
             bool? fileSelected = fileDialog.ShowDialog();
-            
+
             System.Diagnostics.Debug.WriteLine("Dialog closed");
-            
+
             if (fileSelected == true) {
                 System.Diagnostics.Debug.WriteLine("File found");
                 Filter.Filter.LoadFile(fileDialog.FileName);
             }
         }
-    }
-
-    class FilterFile
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
     }
 }
