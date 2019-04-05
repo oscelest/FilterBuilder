1bbe6ca0c815f7f77f48c7b234e4a335eea9d22c
oscelest <oscelest@gmail.com>
2019-04-05 12:27:22 +0200

    Disabled UI code.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,36 +12,36 @@ namespace FilterBuilder {
             InitializeComponent();
         }
 
-        public void Click_LoadFile(object sender, RoutedEventArgs e) {
-            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
-
-            fileDialog.Filter = "filter files (*.filter)|*.filter";
-            fileDialog.InitialDirectory = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents", "My Games", "Path of Exile");
-
-            bool? fileSelected = fileDialog.ShowDialog();
-
-            System.Diagnostics.Debug.WriteLine("Dialog closed");
-
-            if (fileSelected == true) {
-                System.Diagnostics.Debug.WriteLine("File found");
-                Text_Path.Text = fileDialog.FileName;
-                Filter.Filter.LoadFile(fileDialog.FileName);
-                this.Title = "Filter Builder - " + Filter.Filter.FileName;
-                ComboBox_Blocks.ItemsSource = Filter.Filter.Blocks.Select(x => x.Name);
-            }
-        }
-
-        public void Click_SaveFile(object sender, RoutedEventArgs e) {
-            Filter.Filter.SaveFile();
-        }
-
-        public void Selection_FilterBlock(object sender, SelectionChangedEventArgs selectionChangedEventArgs) {
-            Debug.WriteLine(selectionChangedEventArgs.AddedItems[0]);
-        }
-
-        private void Click_AddBlock(object sender, RoutedEventArgs e) {
-            Filter.Filter.Blocks.Add(new Filter.FilterBlock(""));
-            ComboBox_Blocks.ItemsSource = Filter.Filter.Blocks.Select(x => x.Name);
-        }
+//        public void Click_LoadFile(object sender, RoutedEventArgs e) {
+//            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+//
+//            fileDialog.Filter = "filter files (*.filter)|*.filter";
+//            fileDialog.InitialDirectory = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile%"), "Documents", "My Games", "Path of Exile");
+//
+//            bool? fileSelected = fileDialog.ShowDialog();
+//
+//            System.Diagnostics.Debug.WriteLine("Dialog closed");
+//
+//            if (fileSelected == true) {
+//                System.Diagnostics.Debug.WriteLine("File found");
+//                Text_Path.Text = fileDialog.FileName;
+//                Filter.Filter.LoadFile(fileDialog.FileName);
+//                this.Title = "Filter Builder - " + Filter.Filter.FileName;
+//                ComboBox_Blocks.ItemsSource = Filter.Filter.Blocks.Select(x => x.Name);
+//            }
+//        }
+//
+//        public void Click_SaveFile(object sender, RoutedEventArgs e) {
+//            Filter.Filter.SaveFile();
+//        }
+//
+//        public void Selection_FilterBlock(object sender, SelectionChangedEventArgs selectionChangedEventArgs) {
+//            Debug.WriteLine(selectionChangedEventArgs.AddedItems[0]);
+//        }
+//
+//        private void Click_AddBlock(object sender, RoutedEventArgs e) {
+//            Filter.Filter.Blocks.Add(new Filter.FilterBlock(""));
+//            ComboBox_Blocks.ItemsSource = Filter.Filter.Blocks.Select(x => x.Name);
+//        }
     }
 }
