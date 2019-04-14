748b1dd6163950e5bb3c8ea38ba3436d75629067
oscelest <oscelest@gmail.com>
2019-04-14 12:31:12 +0200

    FilterBuilderUpdater v.1.0.0.0 done

--- FilterBuilderUpdater/Program.cs
+++ FilterBuilderUpdater/Program.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace FilterBuilderUpdater {
     internal class Program {
         public static void Main(string[] args) {
-            foreach (var arg in args) {
-                Console.WriteLine(arg);
-            }
+            var processId = int.Parse(args[0]);
+            var downloadPath = args[1];
+            var releasePath = Path.Combine(Path.GetTempPath(), "filter_builder_release.zip");
 
             Console.WriteLine("Closing FilterBuilder process.");
-            Process.GetProcessById(int.Parse(args[0])).Kill();
+            Process.GetProcessById(processId).Kill();
             Console.WriteLine("FilterBuilder process closed. Downloading update.");
-            Environment.GetEnvironmentVariable("LocalAppData");
-            
-            var name = Console.ReadLine();
+            new WebClient().DownloadFile(downloadPath, releasePath);
+            Console.WriteLine("Update downloaded. Removing old version.");
+            foreach (var path in Directory.GetFileSystemEntries(Directory.GetCurrentDirectory())) {
+                if (path.Contains("Updater")) continue;
+                if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)) {
+                    Directory.Delete(path, true);
+                }
+                else {
+                    File.Delete(path);
+                }
+            }
+            Console.WriteLine("Old version removed. Extracting new version.");
+            ZipFile.ExtractToDirectory(releasePath, Directory.GetCurrentDirectory());
+            Console.WriteLine("New version installed. Restarting FilterBuilder.");
+            Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "FilterBuilder.exe"));
         }
     }
 }
